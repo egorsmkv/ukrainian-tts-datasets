a055fe4ab250dbf9f8a5262d458eaedcb7912a99
Yehor Smoliakov <yehors@ukr.net>
2023-08-13 14:53:33 +0000

    Update generate_metadata_file.py
    Former-commit-id: 767f7519379c04eb6979dbf1f8addd04a780c2d4

--- tools/generate_metadata_file.py
+++ tools/generate_metadata_file.py
@@ -1,1 +1,43 @@
-# TBF
+import json
+from os.path import exists, basename
+from glob import glob
+from pathlib import Path
+
+# Config
+OGG_FOLDER = '/home/yehor/kate-data/'
+METADATA_FILE = '/home/yehor/metadata.jsonl'
+
+# Constraints for audios
+MIN_DURATION = 0.1
+MAX_DURATION = 30.0
+
+# Scan the folder for .ogg files
+for filename in glob(OGG_FOLDER + '*.ogg'):
+    # Generate the name of the metadata file
+    json_filename = filename.replace('.ogg', '.json')
+
+    # Skip an item if the metadata file does not exist
+    if not exists(json_filename):
+        continue
+
+    # Load the metadata file
+    data = Path(json_filename).read_text()
+    data_json = json.loads(data)
+
+    sample = data_json['sample']
+
+    # Skip deleted items
+    if sample['is_deleted'] == 1:
+        continue
+
+    json_row = {
+        'file': basename(filename),
+        'orig_text_wo_stress': sample['orig_text_wo_stress'],
+        'orig_text': sample['orig_text'],
+    }
+
+    # Append the row to the metadata file
+    with open(METADATA_FILE, 'a') as f:
+        f.write(json.dumps(json_row) + '\n')
+
+print('Finished')
